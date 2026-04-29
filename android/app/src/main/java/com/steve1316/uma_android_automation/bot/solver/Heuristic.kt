1ec04ef1da1a16f8578483655f922afe8143ea7d
steve1316 <tustev00@gmail.com>
2026-04-28 22:16:48 -0700

    Drop solver beam width to 4 for faster preview

--- android/app/src/main/java/com/steve1316/uma_android_automation/bot/solver/Heuristic.kt
+++ android/app/src/main/java/com/steve1316/uma_android_automation/bot/solver/Heuristic.kt
@@ -11,7 +11,7 @@ package com.steve1316.uma_android_automation.bot.solver
  */
 object Heuristic {
 
-    const val DEFAULT_BEAM_WIDTH: Int = 8
+    const val DEFAULT_BEAM_WIDTH: Int = 4
     const val LAST_TURN: TurnNumber = 72
 
     /**
